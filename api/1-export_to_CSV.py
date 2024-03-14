22c0db94250d73ad0e759a90f57ae144da08606b
Cody <codywalenciak@gmail.com>
2024-03-14 14:23:20 -0500

    Add quoting param to csv.writer

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -19,26 +19,15 @@ if __name__ == '__main__':
     employee = employee_response.json()
     todo_response = requests.get(base_url+user_ext+todo_ext)
     todos = todo_response.json()
-    completed = 0
-    total = 0
-    for todo in todos:
-        total += 1
-        if todo['completed'] is True:
-            completed += 1
-        file = open(file_name, 'w')
-        csv_file = csv.writer(file)
-        count = 0
-        for todo in todos:
-            values = [employee['id'],
-                      employee['username'],
-                      todo['completed'],
-                      todo['title']]
-            csv_file.writerow(values)
-        file.close()
 
-    # print("Employee {0} is done with tasks({1}/{2}):".format(
-    #     employee['name'], completed, total
-    #     ))
-    # for todo in todos:
-    #     if todo['completed'] is True:
-    #         print("\t {}".format(todo['title']))
+    file = open(file_name, 'w')
+    csv_file = csv.writer(file, quoting=csv.QUOTE_ALL)
+    count = 0
+    for todo in todos:
+
+        csv_file.writerow([
+            employee['id'],
+            employee['username'],
+            todo['completed'],
+            todo['title']])
+    file.close()
